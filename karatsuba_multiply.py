9378a4383b0d5e62996f12b2b59afee1cc8af944
bogdan <bkozlovsky@bk.ru>
2020-05-30 20:48:04 +0300

    karatsuba multiply

---
+++ karatsuba_multiply.py
@@ -0,0 +1,42 @@
+from math import log2, ceil
+
+
+def preprocess_digit(first, second):
+    length_first = ceil(log2(len(first)))
+    length_second = ceil(log2(len(second)))
+    length = max(length_first, length_second)
+    length = 2 ** length
+    while len(first) < length:first.insert(0, 0)
+    while len(second) < length:second.insert(0, 0)
+    return length
+
+
+def karatsuba(first, second, length):
+    if len(first) == len(second) == 1:
+        return first[0] * second[0]
+    a = first[:length]
+    b = first[length:]
+    c = second[:length]
+    d = second[length:]
+    ac = karatsuba(a, c, length // 2)
+    bd = karatsuba(b, d, length // 2)
+    a_plus_b = [sum(i) for i in zip(a, b)]
+    d_plus_c = [sum(i) for i in zip(d, c)]
+    abcd = karatsuba(a_plus_b, d_plus_c, length // 2)
+    return ac * 10 ** (2 * length) + (abcd - ac - bd) * 10 ** length + bd
+
+
+def multiply(first, second):
+    length = preprocess_digit(first, second)
+    return karatsuba(first, second, length // 2)
+    
+
+def main():
+    first_digit = [5, 1, 6]
+    second_digit = [7, 3]
+    result = multiply(first_digit, second_digit)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
